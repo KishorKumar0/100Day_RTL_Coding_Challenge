a67fc1aef81178dd0b20d2556317241e83c45c19
KishorKumar0 <144250655+KishorKumar0@users.noreply.github.com>
2023-12-23 17:06:18 +0530

    Update uni_shift_reg_tb.v

--- Day25 - 4 bit Universal Shift Register/uni_shift_reg_tb.v
+++ Day25 - 4 bit Universal Shift Register/uni_shift_reg_tb.v
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: Kishor Kumar
+// Engineer: Kishor Kumsr
 // 
-// Create Date: 17.12.2023 21:12:26
-// Design Name: 4 bit Universal Shift Register
+// Create Date: 19.12.2023 17:24:42
+// Design Name: Universal Shift Register
 // Module Name: uni_shift_reg_tb
 // Project Name: 
 // Target Devices: 
@@ -19,52 +19,40 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+
 module uni_shift_reg_tb();
 
-    reg clk;
-    reg clear;
-    reg [1:0] S;
-    reg [3:0] I;
-    reg SIL;
-    reg SIR;
-
-    wire [3:0] Out;
-
-    uni_shift_reg uut(
-        .Out(Out),
-        .clk(clk),
-        .clear(clear),
-        .S(S),
-        .I(I),
-        .SIL(SIL),
-        .SIR(SIR)
-    );
-
-    initial begin
-        $dumpfile("uni_shift_reg_tb.vcd");
-        $dumpvars(0, uni_shift_reg_tb);
-
-        // Initialize inputs
-        clk = 0;
-        clear = 1;
-        S = 2'b00;
-        I = 4'b0000;
-        SIL = 0;
-        SIR = 1;
-
-        // Apply initial conditions
-        #10 clear = 0;
-
-        // Clock toggling and input changes
-        repeat (10) begin
-            #5 clk = ~clk;
-        end
-
-        // Add more test scenarios as needed
-
-        #100 $finish;
-    end
-
-    always #5 clk = ~clk; // Clock toggling
-
+  reg clk, rst_n;
+  reg [1:0] select;
+  reg [3:0] p_din;
+  reg s_left_din, s_right_din;
+  wire [3:0] p_dout; //parallel data out
+  wire s_left_dout, s_right_dout;
+  
+  uni_shift_reg usr(clk, rst_n, select, p_din, s_left_din, s_right_din, p_dout, s_left_dout, s_right_dout);
+  
+  always #2 clk = ~clk;
+  initial begin
+    $monitor("select=%b, p_din=%b, s_left_din=%b, s_right_din=%b --> p_dout = %b, s_left_dout = %b, s_right_dout = %b",select, p_din, s_left_din, s_right_din, p_dout, s_left_dout, s_right_dout);
+    clk = 0; rst_n = 0;
+    #3 rst_n = 1;
+    
+    p_din = 4'b1101;
+    s_left_din = 1'b1;
+    s_right_din = 1'b0;
+    
+    select = 2'h3; #10;
+    select = 2'h1; #20;
+    p_din = 4'b1101;
+    select = 2'h3; #10;
+    select = 2'h2; #20;
+    select = 2'h0; #20;
+    
+    $finish;
+  end
+  // To enable waveform
+  initial begin 
+    $dumpfile("dump.vcd"); $dumpvars;
+  end
+   
 endmodule
