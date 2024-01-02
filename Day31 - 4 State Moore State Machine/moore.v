e6e1942f3bc2153da7d4d1aae7a33a02956004fc
KishorKumar0 <144250655+KishorKumar0@users.noreply.github.com>
2024-01-02 23:12:44 +0530

    Update moore.v

--- Day31 - 4 State Moore State Machine/moore.v
+++ Day31 - 4 State Moore State Machine/moore.v
@@ -1,1 +1,75 @@
-#include
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Kishor Kumar
+// 
+// Create Date: 02.01.2024 01:44:00
+// Design Name: Moore State Machine
+// Module Name: moore
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module moore(clk, inbit, reset, detect);
+
+    input clk;
+    input reset;
+    input inbit;
+    output reg detect;
+    
+    reg [3:0]state;
+    
+    initial begin
+        state <=2'b00;
+    end 
+    always @(posedge clk, posedge reset)
+    begin
+    if(reset)
+    state <= 2'b00;
+    else
+    begin
+        case(state)
+        2'b00:
+        begin
+            if(inbit) state <= 2'b01;
+            else      state <= 2'b10;
+        end
+        2'b01:
+        begin
+            if(inbit) state <= 2'b11;
+            else      state <= 2'b10;
+        end
+        2'b10:
+        begin
+            if(inbit) state <= 2'b01;
+            else      state <= 2'b11;
+       end
+       2'b11:
+       begin
+            if(inbit) state <= 2'b01;
+            else      state <= 2'b10;
+       end
+       endcase
+    end
+    end
+    
+    always @(posedge clk, posedge reset)
+    begin
+        if(reset)
+            detect <= 0;
+        else if(state == 2'b11)
+            detect <= 1;
+        else detect <= 0;
+    end
+     
+endmodule
