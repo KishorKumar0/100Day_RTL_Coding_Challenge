3e955c79ad43a8df7156329ae0356670d7a1c960
KishorKumar0 <144250655+KishorKumar0@users.noreply.github.com>
2024-01-04 22:03:18 +0530

    Create ram.v

---
+++ Synchronous RAM/ram.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Kishor Kumar
+// 
+// Create Date: 04.01.2024 21:53:30
+// Design Name: 1 kilo byte Syncronous Ram
+// Module Name: ram
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ram(
+    input clk,
+    input write_enable,
+    input [9:0]address,
+    input [7:0]data_in,
+    output reg [7:0]data_out
+);
+
+reg [7:0]ram_block[0:1023];
+
+always @(posedge clk) begin
+        if(write_enable)
+            ram_block[address] <= data_in;
+        else
+            data_out <= ram_block[address];
+end
+
+endmodule
