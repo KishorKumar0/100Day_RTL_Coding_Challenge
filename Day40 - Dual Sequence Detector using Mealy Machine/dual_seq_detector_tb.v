450b2b51672700b5da09781fde38da7e730f92c0
KishorKumar0 <144250655+KishorKumar0@users.noreply.github.com>
2024-03-12 00:56:05 +0530

    Update dual_seq_detector_tb.v

--- Day40 - Dual Sequence Detector using Mealy Machine/dual_seq_detector_tb.v
+++ Day40 - Dual Sequence Detector using Mealy Machine/dual_seq_detector_tb.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: Kishor Kumar
 // 
-// Create Date: 12.03.2024 00:13:39
+// Create Date: 12.03.2024 00:16:25
 // Design Name: Dual Sequence Detector using Mealy Machine(101 and 0110)
-// Module Name: dual_seq_detector
+// Module Name: dual_seq_detector_tb
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,95 +20,39 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module dual_seq_detector(input din,clk,reset,
-	output reg y);
-	
-	reg[2:0] cst, nst;	
-	parameter S0=3'b000,
-              S1=3'b001,
-              S2=3'b010,
-              S3=3'b011,
-              S4=3'b100,
-              S5=3'b101;
-				 
-	always@ (posedge clk)
-	begin
-		if(reset)
-			cst<=S0;
-		else
-			cst<=nst;
-	end
-	
-	always @(cst or din)
-	begin
-		case(cst)
-			S0: if(din==1'b1)
-					begin
-						nst<=S1;
-						y<=1'b0;
-					end
-				 else
-					begin
-						nst<=S2;
-						y<=1'b0;
-					end
-				
-			S1: if(din==1'b1)
-					begin
-						nst<=S1;
-						y<=1'b0;
-					end
-				 else
-					begin
-						nst<=S3;
-						y<=1'b0;
-					end
-					
-			S2: if(din==1'b1)
-					begin
-						nst<=S4;
-						y<=1'b0;
-					end
-				 else
-					begin
-						nst<=S2;
-						y<=1'b0;
-					end
-					
-			S3: if(din==1'b1)
-					begin
-						nst<=S4;
-						y<=1'b1;
-					end
-				 else
-					begin
-						nst<=S2;
-						y<=1'b0;
-					end
-			
-		    S4: if(din==1'b1)
-					begin
-						nst<=S5;
-						y<=1'b0;
-					end
-				 else
-					begin
-						nst<=S3;
-						y<=1'b0;
-					end
-					
-			S5: if(din==1'b1)
-					begin
-						nst<=S1;
-						y<=1'b0;
-					end
-				 else
-					begin
-						nst<=S3;
-						y<=1'b1;
-					end
-					
-		default: nst<=S0;
-		endcase
-	end
+module dual_seq_detector_tb();
+reg din,clk,reset;
+wire y;
+
+dual_seq_detector dut(din,clk,reset,y);
+
+initial begin
+clk= 1'b0;
+forever #5 clk= ~clk;
+end
+
+initial begin
+reset= 1'b1;
+#10 reset= 1'b0;
+#5 din= 1'b0;
+#10 din= 1'b1;
+#10 din= 1'b1;
+#10 din= 1'b0;
+#10 din= 1'b1;
+#10 din= 1'b1;
+#10 din= 1'b0;
+#10 din= 1'b0;
+#10 din= 1'b1;
+#10 din= 1'b0;
+#10 din= 1'b1;
+#10 din= 1'b0;
+#10 din= 1'b1;
+#10 din= 1'b1;
+#10 din= 1'b0;
+end
+
+initial begin
+$monitor("\t\t clock: %d  input: %d   detect: %d",clk, din, y);
+#180 $finish;
+end
 endmodule
